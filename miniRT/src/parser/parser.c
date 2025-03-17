6ae0137d24a172fd65f48cf37c813e208b16522b
Nzola Kiampava <nzolakiampava@gmail.com>
2025-03-17 10:23:48 +0100

    Update parser.c

--- miniRT/src/parser/parser.c
+++ miniRT/src/parser/parser.c
@@ -3,24 +3,49 @@
 /*                                                        :::      ::::::::   */
 /*   parser.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nkiampav <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: nkiampav <nkiampav@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/08 12:00:15 by nkiampav          #+#    #+#             */
-/*   Updated: 2025/03/08 12:00:19 by nkiampav         ###   ########.fr       */
+/*   Created: 2025/02/27 09:21:04 by nkiampav          #+#    #+#             */
+/*   Updated: 2025/03/17 10:22:56 by nkiampav         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minirt.h"
 
-static int	parse_line(char **elements, t_scene *scene);
+static 	int parse_line(char **elements, t_scene *scene);
+
+static char	*read_line(int fd)
+{
+	char	buffer[4096];
+	char	*line;
+	int		i;
+	int		bytesread;
+
+	i = 0;
+	bytesread = read(fd, &buffer[i], 1);
+	if (bytesread <= 0)
+		return (NULL);
+
+	while (bytesread > 0 && buffer[i] != '\n')
+	{
+		i++;
+		if (i >= 4095)
+			break ;
+		bytesread = read(fd, &buffer[i], 1);
+	}
+
+	buffer[i] = '\0';
+	line = ft_strdup(buffer);
+	return (line);
+}
 
 /**
  * Parses a scene file and fills the scene structure
- * Returns 0 on success, -1 on error
- */
+ * Returns 0 on sucess, -1 on error
+*/
 int	parse_scene(char *filename, t_scene *scene)
 {
-	int		fd;
+	int	fd;
 	char	*line;
 	char	**elements;
 	int		ret;
@@ -32,12 +57,12 @@ int	parse_scene(char *filename, t_scene *scene)
 		return (print_error(ERR_FILE), -1);
 	scene_init(scene);
 	ret = 0;
-	while (ret == 0 && get_next_line(fd, &line) > 0)
+	while (ret == 0 && (line = read_line(fd)) != NULL)
 	{
 		if (line[0] == '\0' || line[0] == '#')
 		{
 			free(line);
-			continue;
+			continue ;
 		}
 		elements = split_line(line);
 		if (!elements)
@@ -58,7 +83,7 @@ int	parse_scene(char *filename, t_scene *scene)
 /**
  * Parse a line from the scene file
  * Returns 0 on success, -1 on error
- */
+*/
 static int	parse_line(char **elements, t_scene *scene)
 {
 	if (!elements[0])
@@ -82,7 +107,7 @@ static int	parse_line(char **elements, t_scene *scene)
 /**
  * Split a line into elements
  * Returns a NULL-terminated array of strings, or NULL on error
- */
+*/
 char	**split_line(char *line)
 {
 	char	**elements;
@@ -102,7 +127,7 @@ char	**split_line(char *line)
 			i++;
 		if (!line[i])
 			break;
-		// Mark start of element
+		// Mark end of element
 		start = i;
 		// Find end of element
 		while (line[i] && line[i] != ' ' && line[i] != '\t')
@@ -122,7 +147,7 @@ char	**split_line(char *line)
 
 /**
  * Count the number of elements in a NULL-terminated array
- */
+*/
 int	count_elements(char **elements)
 {
 	int	count;
@@ -135,16 +160,16 @@ int	count_elements(char **elements)
 
 /**
  * Free a NULL-terminated array of strings
- */
+*/
 void	free_split(char **split)
 {
 	int	i;
-
+	
 	i = 0;
 	while (split[i])
 	{
-		free(split[i]);
-		i++;
+		if(split[i])
+			i++;
 	}
 	free(split);
 }
@@ -152,16 +177,16 @@ void	free_split(char **split)
 /**
  * Validate the scene configuration
  * Returns 0 on success, -1 on error
- */
+*/
 int	validate_scene(t_scene *scene)
 {
 	// Verify camera orientation is normalized
 	if (fabs(vec3_length(scene->camera.orientation) - 1.0) > EPSILON)
 		scene->camera.orientation = vec3_normalize(scene->camera.orientation);
-	
+
 	// Check if camera FOV is valid
 	if (scene->camera.fov <= 0 || scene->camera.fov >= 180)
 		return (print_error("Invalid camera FOV\n"), -1);
-	
+
 	return (0);
 }
