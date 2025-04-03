52d0ae9d86ebf85c6650e3217d1f8becab9fad6c
Manuel Mazunga De Almeida <maalmeid@c1r1s1.42luanda.com>
2025-04-03 22:20:56 +0100

    is miss 3 error

--- includes/ray.h
+++ includes/ray.h
@@ -6,7 +6,7 @@
 /*   By: nkiampav <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/27 09:19:44 by nkiampav          #+#    #+#             */
-/*   Updated: 2025/02/27 09:19:46 by nkiampav         ###   ########.fr       */
+/*   Updated: 2025/04/03 22:18:24 by maalmeid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,33 +18,32 @@
 
 typedef struct s_ray
 {
-    t_vec3  origin;
-    t_vec3  direction;
-}   t_ray;
+	t_vec3	origin;
+	t_vec3	direction;
+}	t_ray;
 
-typedef struct  s_hit_info
+typedef struct s_hit_info
 {
-    t_vec3  point;                   // Intersection point
-    t_vec3  normal;                  // Surface normal at intersection
-    double  t;                       // Distance along ray
-    t_color color;                   // Object color
-    void    *object;                 // Pointer to hit object
-    int     type;                    // Type of the hit object
-}   t_hit_info;
-
+	t_vec3	point;
+	t_vec3	normal;
+	double	t;
+	t_color	color;
+	void	*object;
+	int		type;
+}	t_hit_info;
 
 // Ray functions
-t_ray ray_create(t_vec3 origin, t_vec3 direction);
-t_vec3 ray_at(t_ray ray, double t);
-t_ray   ray_transformation(t_ray ray, t_vec3 translation);
+t_ray	ray_create(t_vec3 origin, t_vec3 direction);
+t_vec3	ray_at(t_ray ray, double t);
+t_ray	ray_transformation(t_ray ray, t_vec3 translation);
 
 // Ray object intersection function
-int ray_intersect_sphere(t_ray ray, void *sphere, t_hit_info *hit);
-int ray_intersect_plane(t_ray ray, void *plane, t_hit_info *hit);
-int ray_intersect_cylinder(t_ray ray, void *cylinder, t_hit_info *hit);
-int ray_intersect_any(t_ray ray, void **object, int num_objects, t_hit_info *hit);
+int		ray_intersect_sphere(t_ray ray, void *sphere, t_hit_info *hit);
+int		ray_intersect_plane(t_ray ray, void *plane, t_hit_info *hit);
+int		ray_intersect_cylinder(t_ray ray, void *cylinder, t_hit_info *hit);
+int		ray_intersect_any(t_ray ray, void **object, int num_objects, t_hit_info *hit);
 
 // Camera ray generation
-t_ray   camera_ray(t_vec3 origin, t_vec3 directoin, double fov, double aspect_ratio, double x, double y);
+t_ray	camera_ray(t_vec3 origin, t_vec3 directoin, double fov, double aspect_ratio, double x, double y);
 
 #endif
