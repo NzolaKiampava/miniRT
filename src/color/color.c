d989059bd9e5eb0a620e0635bd85dabe7b57a820
Manuel Mazunga De Almeida <maalmeid@c1r8s5.42luanda.com>
2025-04-17 15:35:52 +0100

    fixed color

--- src/color/color.c
+++ src/color/color.c
@@ -6,82 +6,85 @@
 /*   By: nkiampav <nkiampav@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/27 09:51:16 by nkiampav          #+#    #+#             */
-/*   Updated: 2025/04/04 11:17:37 by nkiampav         ###   ########.fr       */
+/*   Updated: 2025/04/17 15:34:58 by maalmeid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minirt.h"
 
-t_color int_to_color(int rgb)
+t_color	int_to_color(int rgb)
 {
-    t_color c;
+	t_color	c;
 
-    // Extract RGB components from integer
-    c.r = (rgb >> 16) & 0xFF;
-    c.g = (rgb >> 8) & 0xFF;
-    c.b = rgb & 0xFF;
-    return (c);
+	c.r = (rgb >> 16) & 0xFF;
+	c.g = (rgb >> 8) & 0xFF;
+	c.b = rgb & 0xFF;
+	return (c);
 }
 
-double  color_to_scalar(t_color c)
+double	color_to_scalar(t_color c)
 {
-    // Convert RGB color to a scalar value (usually average of components)
-    // This is commonly used in lighting used in lighting calculations
-    return ((double)(c.r + c.g + c.b) / (3.0 * 255.0));
+	return ((double)(c.r + c.g + c.b) / (3.0 * 255.0));
 }
 
 // Color utilities
-t_color color_clamp(t_color c)
+t_color	color_clamp(t_color c)
 {
-    t_color result;
+	t_color	result;
 
-    // Clamp RGB values to 0-255 range
-    result.r = (c.r < 0) ? 0 : ((c.r > 255) ? 255 : c.r);
-    result.g = (c.g < 0) ? 0 : ((c.g > 255) ? 255 : c.g);
-    result.b = (c.b < 0) ? 0 : ((c.b > 255) ? 255 : c.b);
-    return (result);
+	if (c.r < 0)
+		result.r = 0;
+	else if (c.r > 255)
+		result.r = 255;
+	else
+		result.r = c.r;
+	if (c.g < 0)
+		result.g = 0;
+	else if (c.g > 255)
+		result.g = 255;
+	else
+		result.r = c.g;
+	if (c.b < 0)
+		result.b = 0;
+	else if (c.b > 255)
+		result.b = 255;
+	else
+		result.b = c.b;
+	return (result);
 }
 
-t_color color_lerp(t_color c1, t_color c2, double t)
+t_color	color_lerp(t_color c1, t_color c2, double t)
 {
-    t_color result;
+	t_color	result;
 
-    // Linear interpolation between two colors
-    if (t < 0)
-        t = 0;
-    if (t > 1)
-        t = 1;
-    result.r = (int)(c1.r * (1 - t) + c2.r * t);
-    result.g = (int)(c1.g * (1 - t) + c2.g * t);
-    result.b = (int)(c1.b * (1 - t) + c2.b * t);
-    return (result);
+	if (t < 0)
+		t = 0;
+	if (t > 1)
+		t = 1;
+	result.r = (int)(c1.r * (1 - t) + c2.r * t);
+	result.g = (int)(c1.g * (1 - t) + c2.g * t);
+	result.b = (int)(c1.b * (1 - t) + c2.b * t);
+	return (result);
 }
 
 // Parse color from string "r,g,b" format
-t_color color_parse(char *str)
+t_color	color_parse(char *str)
 {
-    t_color color;
-    char    *token;
-    char    *saveptr;
+	t_color	color;
+	char	*token;
+	char	*saveptr;
 
-    color = color_black();
-    if (!str)
-        return (color);
-
-    // Parse red component
-    token = strtok_r(str, ",", &saveptr);
-    if (token)
-        color.r = ft_atoi(token);
-
-    // Parse green component
-    token = strtok_r(NULL, ",", &saveptr);
-    if (token)
-        color.g = ft_atoi(token);
-
-    // Parse blue component
-    token = strtok_r(NULL, ",", &saveptr);
-    if (token)
-        color.b = ft_atoi(token);
-    
-    return (color_clamp(color));
+	color = color_black();
+	if (!str)
+		return (color);
+	token = strtok_r(str, ",", &saveptr);
+	if (token)
+		color.r = ft_atoi(token);
+	token = strtok_r(NULL, ",", &saveptr);
+	if (token)
+		color.g = ft_atoi(token);
+	token = strtok_r(NULL, ",", &saveptr);
+	if (token)
+		color.b = ft_atoi(token);
+	return (color_clamp(color));
 }
