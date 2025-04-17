17764d4921bfa11c779003501a67d5249cf371ef
Manuel Mazunga De Almeida <maalmeid@c1r8s5.42luanda.com>
2025-04-17 16:09:34 +0100

    done color_utils

--- src/color/color_utils.c
+++ src/color/color_utils.c
@@ -6,57 +6,56 @@
 /*   By: nkiampav <nkiampav@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/04 11:17:46 by nkiampav          #+#    #+#             */
-/*   Updated: 2025/04/04 11:18:13 by nkiampav         ###   ########.fr       */
+/*   Updated: 2025/04/17 16:08:27 by maalmeid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minirt.h"
 
 // Color creation
-t_color color_create(int r, int g, int b)
+t_color	color_create(int r, int g, int b)
 {
-    t_color color;
+	t_color	color;
 
-    color.r = r;
-    color.g = g;
-    color.b = b;
-    return (color);
+	color.r = r;
+	color.g = g;
+	color.b = b;
+	return (color);
 }
 
 // Basic color operations
-t_color color_add(t_color c1, t_color c2)
+t_color	color_add(t_color c1, t_color c2)
 {
-    t_color result;
+	t_color	result;
 
-    result.r = c1.r + c2.r;
-    result.g = c1.g + c2.g;
-    result.b = c1.b + c2.b;
-    return (color_clamp(result));
+	result.r = c1.r + c2.r;
+	result.g = c1.g + c2.g;
+	result.b = c1.b + c2.b;
+	return (color_clamp(result));
 }
 
-t_color color_multiply(t_color c, double scalar)
+t_color	color_multiply(t_color c, double scalar)
 {
-    t_color result;
+	t_color	result;
 
-    result.r = (int)(c.r * scalar);
-    result.g = (int)(c.g * scalar);
-    result.b = (int)(c.b * scalar);
-    return (color_clamp(result));
+	result.r = (int)(c.r * scalar);
+	result.g = (int)(c.g * scalar);
+	result.b = (int)(c.b * scalar);
+	return (color_clamp(result));
 }
 
-t_color color_multiply_colors(t_color c1, t_color c2)
+t_color	color_multiply_colors(t_color c1, t_color c2)
 {
-    t_color result;
+	t_color	result;
 
-    result.r = (c1.r * c2.r) / 255;
-    result.g = (c1.g * c2.g) / 255;
-    result.b = (c1.b * c2.b) / 255;
-    return (result);
+	result.r = (c1.r * c2.r) / 255;
+	result.g = (c1.g * c2.g) / 255;
+	result.b = (c1.b * c2.b) / 255;
+	return (result);
 }
 
 // Color conversion
-int color_to_int(t_color c)
+int	color_to_int(t_color c)
 {
-    // Convert RGB components to a single integer (0xRRGGBB format)
-    return ((c.r & 0xFF) << 16) | ((c.g & 0xFF) << 8) | (c.b & 0xFF);
+	return (((c.r & 0xFF) << 16) | ((c.g & 0xFF) << 8) | (c.b & 0xFF));
 }
