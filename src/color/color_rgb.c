cd31e122fdb8575fe2e667a943a16873cf7447b5
Manuel Mazunga De Almeida <maalmeid@c1r8s5.42luanda.com>
2025-04-17 15:56:26 +0100

    all thing done color_rgb

--- src/color/color_rgb.c
+++ src/color/color_rgb.c
@@ -6,34 +6,34 @@
 /*   By: nkiampav <nkiampav@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/04 11:16:07 by nkiampav          #+#    #+#             */
-/*   Updated: 2025/04/04 11:16:24 by nkiampav         ###   ########.fr       */
+/*   Updated: 2025/04/17 15:55:18 by maalmeid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minirt.h"
 
 // Predefined colors
-t_color color_black(void)
+t_color	color_black(void)
 {
-    return (color_create(0, 0, 0));
+	return (color_create(0, 0, 0));
 }
 
-t_color color_white(void)
+t_color	color_white(void)
 {
-    return (color_create(255, 255, 255));
+	return (color_create(255, 255, 255));
 }
 
-t_color color_red(void)
+t_color	color_red(void)
 {
-    return (color_create(255, 0, 0));
+	return (color_create(255, 0, 0));
 }
 
-t_color color_green(void)
+t_color	color_green(void)
 {
-    return (color_create(0, 255, 0));
+	return (color_create(0, 255, 0));
 }
 
-t_color color_blue(void)
+t_color	color_blue(void)
 {
-    return (color_create(0, 0, 255));
+	return (color_create(0, 0, 255));
 }
